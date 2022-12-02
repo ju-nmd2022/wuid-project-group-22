a19c5f190464d7e150581c6277f256c5cfbd91dc
opju21 <117085376+opju21@users.noreply.github.com>
2022-12-02 19:38:41 +0100

    added menu star .... yay ;-;

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-function menuPopup() {}
+const popoutMenu = document.querySelector(".popout-menu-container");
+const menuButton = document.querySelector(".menu");
+const closeMenuButton = document.querySelector(".close-menu-button");
+popoutMenu.style.visibility = "hidden";
+menuButton.style.visibility = "visible";
+closeMenuButton.style.visibility = "hidden";
+
+menuButton.addEventListener("click", function (event) {
+  menuButton.style.visibility = "hidden";
+  popoutMenu.style.visibility = "visible";
+  closeMenuButton.style.visibility = "visible";
+});
+closeMenuButton.addEventListener("click", function (event) {
+  menuButton.style.visibility = "visible";
+  popoutMenu.style.visibility = "hidden";
+  closeMenuButton.style.visibility = "hidden";
+});
