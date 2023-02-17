04f2a545362f483467b819726dc2d41bf36f1fe9
danae1moreault <117374799+danae1moreault@users.noreply.github.com>
2023-02-17 09:32:19 -0500

    Add files via upload

---
+++ addition.js
@@ -0,0 +1,12 @@
+document.getElementById("changetxt").addEventListener("click", colourmod);
+// defining the click as action
+
+function colourmod() {
+  document.getElementById("changetxt").style.color = "#cfd3a5";
+  document.getElementById("txt").innerText =
+    "Thank you for subscribing, expect to hear from us shortly!";
+  // adding content to the empty text zone^
+  document.getElementById("txt").innerHTML;
+  txt.style.color = "#7c7777";
+  // changing text colour of the subscribe button
+}
